ac7bdca833a3ab13ea27a64de9681191ed05497e
bkmalan <55806149+bkmalan@users.noreply.github.com>
2019-10-02 13:30:04 +0530

    Update IPmatrix.c

--- IPmatrix.c
+++ IPmatrix.c
@@ -2,14 +2,21 @@
 
 int main()
 {
-	int n = 4,row[4],bk[4][4] = {96,144,48,240,94,141,47,235,1313,1515,1212,606,3185,3675,2940,1470};
-	for(int i = 0; i < n; i++)	{	
-		for(int j= 0; j < n; j++)		
-			printf("%d ",bk[i][j]);
-			row[i] = (bk[i][0]^bk[i][1]) - (bk[i][2]&bk[i][3]);
-		printf("\n");
-	}		
-	printf("The IP address is: %d.%d.%d.%d\n",row[0],row[1],row[2],row[3]);	
-	
+  long n = 4,row[4],bk[4][4];
+  for(int i = 0; i < n; i++)
+    for(int j= 0; j < n; j++)
+    scanf("%ld",&bk[i][j]);
+  printf("===============\n");
+  for(int i = 0; i < n; i++)  { 
+    for(int j= 0; j < n; j++)   
+      printf("%d ",bk[i][j]);
+      row[i] = (bk[i][0]^bk[i][1]) - (bk[i][2]&bk[i][3]);
+    printf("\n");
+  }   
+  printf("The IP address is: %d.%d.%d.%d\n",row[0],row[1],row[2],row[3]); 
+  
+  if((row[0] == 192) && (row[1] == 168 )&&( row[2] == 174 )&&( row[3] == 238))
+    printf("Good Mat\n");
+
   return 0;
-}	
+}
